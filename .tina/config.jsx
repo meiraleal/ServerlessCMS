c17a1a71a0e60e586343677cf777562c3fb961b8
Alan Meira <alanmeira@gmail.com>
2023-04-02 22:15:16 -0300

    update

--- .tina/config.jsx
+++ .tina/config.jsx
@@ -820,8 +820,8 @@ const PagesCollection = {
 
 export default defineConfig({
   branch,
-  clientId: "ec80bfa2-69ad-4167-af8a-964c9609c8bf", // Get this from tina.io
-  token: "dd362ccf58b166176f0b818999c405cd90b8d1fb", // Get this from tina.io
+  clientId: "9eaa0998-03a0-4fc3-af03-71901b9820ac", // Get this from tina.io
+  token: "58fdb71a1c99e0bbe0e6004eca5a550ede99100a", // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "static",
